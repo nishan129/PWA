d77b7d7de2386965c94b22c0b37673f59e9c6594
nishant129 <nishantborkar139@gmail.com>
2024-07-27 19:05:25 +0530

    add next auth for protecting pages

--- context/Providers.jsx
+++ context/Providers.jsx
@@ -6,9 +6,10 @@ import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "../app/api/uploadthing/core";
 
-import {SessionProvider} from "next-auth/react";
+// import {SessionProvider} from "next-auth/react";
 import { Provider } from 'react-redux';
 import { store } from '@/redux/store';
+import { SessionProvider } from 'next-auth/react';
 
 
 export default function Providers({children}) {
@@ -20,9 +21,11 @@ export default function Providers({children}) {
         <NextSSRPlugin
           routerConfig={extractRouterConfig(ourFileRouter)}
         />   
+        <SessionProvider>
        <Provider store={store}>
        {children}
        </Provider>
+       </SessionProvider>
     </ThemeProvider>
   );
 }
