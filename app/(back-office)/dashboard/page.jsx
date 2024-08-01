0606ddf86ee9ab9710e132ec5af7888fc12a9a0d
nishant129 <nishantborkar139@gmail.com>
2024-08-01 23:52:56 +0530

    update the dashboard

--- app/(back-office)/dashboard/page.jsx
+++ app/(back-office)/dashboard/page.jsx
@@ -1,13 +1,11 @@
-
 import Heading from '@/components/backoffice/Heading';
 import LargeCards from '@/components/backoffice/LargeCards';
 import SmallCards from '@/components/backoffice/SmallCards';
 import DashboardCharts from '@/components/backoffice/DashboardCharts';
 import React from 'react';
+// import CustomDataTable from '@/components/backoffice/CustomDataTable';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/authOptions';
-import KiranaDashboard from '@/components/backoffice/KiranaDashboard';
-import WholesalerDashgboard from '@/components/backoffice/WholesalerDashgboard';
 import { getData } from '@/lib/getData';
 import OrderDetails from '@/components/backoffice/OrderDetails';
 
@@ -17,14 +15,7 @@ export default async function page() {
   const sales = await getData("sale")
   const products = await getData("products");
   const orders  = await getData("orders");
-  
-  
-  if(role==="KIRANA"){
-    return <KiranaDashboard />;
-  }
-  if (role==="WHOLESALER"){
-    return <WholesalerDashgboard />;
-  }
+
   return (
     <div>
         <Heading title="Dashboard Overview"/>
@@ -35,8 +26,7 @@ export default async function page() {
         {/* Charts */}
         <DashboardCharts sales={sales} orders={orders}/>
         {/* Recent Orders Table */}
-        {/* <CustomDataTable />  */}
-        <OrderDetails sales={sales} orders={orders} />
+        <OrderDetails />
     </div>
   );
 }
--- components/backoffice/OrderDetails.jsx
+++ components/backoffice/OrderDetails.jsx
@@ -1,9 +1,11 @@
-
+import { getData } from '@/lib/getData';
 import React from 'react';
 import {convertIsoDateToNormal} from "@/lib/convertisoDatetoNormal";
 import Link from 'next/link';
 
-export default async function OrderDetails({sales, orders}) {
+export default async function OrderDetails() {
+    const orders = await getData("orders");
+    const sales = await getData("sale");
 
     return (
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 py-8 dark:text-slate-50 text-slate-800'>
@@ -14,6 +16,7 @@ export default async function OrderDetails({sales, orders}) {
                     <div key={order.id} className='border border-green-500  rounded-lg dark:text-white shadow-lg hover:shadow-xl p-6 flex flex-col'>
                         <p><strong>Order No: </strong> {order.orderNumber}</p>
                         <p><strong>Store Name:</strong> {order.storename}</p>
+                        <p><strong>Payment Method:</strong> {order.paymentMethod}</p>
                         <p><strong>Order Status:</strong> {order.orderStatus}</p>
                         {orderSale && (
                             <div>
@@ -21,6 +24,7 @@ export default async function OrderDetails({sales, orders}) {
                                 <p><strong>Sale Date:</strong> {convertIsoDateToNormal(orderSale.createdAt)}</p>
                             </div>
                         )}
+
                             <p><strong>Order Items:</strong> {order.orderItems.length}</p>
                     </div>
                     </Link>
