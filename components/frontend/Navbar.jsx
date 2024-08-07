96b0369252f122d589eb5754fbcf5c267f84ed51
nishant129 <nishantborkar139@gmail.com>
2024-08-07 13:10:31 +0530

    change navbar color

--- components/frontend/Navbar.jsx
+++ components/frontend/Navbar.jsx
@@ -10,7 +10,7 @@ import CartCount from './CartCount';
 
 export default function Navbar() {
   return (
-    <div className="w-full fixed top-0 z-50 bg-green-400 dark:bg-green-400 shadow-lg rounded-br-lg rounded-bl-lg">
+    <div className="w-full fixed top-0 z-50 bg-green-200 dark:bg-green-200 shadow-lg rounded-br-lg rounded-bl-lg">
       <div className="flex items-center justify-between py-4 max-w-7xl mx-auto px-8 gap-4">
         {/* Search */}
         <Link href={"/search"} className="flex-grow">
