30a57d05e874e0210a55a73477dbf4f2bfb0ae69
nishant129 <nishantborkar139@gmail.com>
2024-08-07 16:02:34 +0530

    update categorylist.jsx change the ui

--- components/frontend/CategoryList.jsx
+++ components/frontend/CategoryList.jsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 import CategoryCarousel from './CategoryCarousel';
+import { ChevronRight } from 'lucide-react';
 
 export default function CategoryList({ categories }) {
 
@@ -11,13 +12,13 @@ export default function CategoryList({ categories }) {
   return (
     <div>
       {categorie.map((category, index) => (
-        <div key={index} className='border border-green-600 rounded-lg  text-slate-800 overflow-hidden mb-4'>
-          <div className="bg-slate-100 dark:bg-slate-100 py-1 px-6 
-          font-semibold border-b border-green-600 dark:border-green-600 text-slate-800 dark:text-slate-800
+        <div key={index} className='rounded-lg  text-slate-800 overflow-hidden mb-4'>
+          <div className="bg-slate-100 dark:bg-slate-100 py-1 px-2
+          font-semibold  text-slate-800 dark:text-slate-800
           flex justify-between items-center">
             <h2>{category.title}</h2>
-            <Link href={`/category/${category.slug}`} className='bg-green-600 text-slate-50 rounded-md px-4  
-            py-2 ' >See All</Link>
+            <Link href={`/category/${category.slug}`} className='bg-green-600 text-slate-50 rounded-full px-1  
+            py-1 ' ><ChevronRight /></Link>
           </div>
           <div className='dark:bg-slate-100'>
             <CategoryCarousel products={category.products}/>
