b059df8f4bbc530e97f8393fb662e39c264624ff
nishant129 <nishantborkar139@gmail.com>
2024-08-10 16:36:15 +0530

    update the last banner

--- app/(front-end)/page.js
+++ app/(front-end)/page.js
@@ -55,7 +55,7 @@ export default function Home() {
         )
       ))}
 
-      <Banner />
+      <Banner activeMarket={activeMarket} />
     </div>
   );
 }
--- components/frontend/Banner.jsx
+++ components/frontend/Banner.jsx
@@ -3,23 +3,27 @@ import Image from 'next/image';
 import banner from "../../public/banner.png";
 import ExploreProduct from './explor';
 
-export default function Banner() {
+export default function Banner({ activeMarket }) {
     const [showExplore, setShowExplore] = useState(false);
 
     const handleClick = () => {
         setShowExplore(true);
-    }
+    };
 
     return (
-        <div>
-            <div onClick={handleClick} className='mb-2'>
-                <Image 
-                    src={banner}
-                    className='rounded-lg'
-                    alt="Banner Image"
-                />
-            </div>
-            {showExplore && <ExploreProduct />}
-        </div>
+        <>
+            {activeMarket === "FMCG" && (
+                <div>
+                    <div onClick={handleClick} className='mb-2'>
+                        <Image 
+                            src={banner}
+                            className='rounded-lg'
+                            alt="Banner Image"
+                        />
+                    </div>
+                    {showExplore && <ExploreProduct />}
+                </div>
+            )}
+        </>
     );
 }
