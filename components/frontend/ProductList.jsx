c628c5d12843427e1966793a2fae39f4aae1dbe2
nishant129 <nishantborkar139@gmail.com>
2024-08-17 16:42:23 +0530

    update the product list

--- components/frontend/ProductList.jsx
+++ components/frontend/ProductList.jsx
@@ -78,7 +78,7 @@ export default function ProductList({ slug, id }) {
                   <div className="text-lg font-semibold text-slate-800 flex items-baseline">
                     ₹{showDiscount ? product.discountedPrice : product.product_price}
                     {showDiscount && <span className="text-sm font-normal text-slate-800 ml-1">MRP</span>}
-                    {showDiscount && <span className="text-sm text-gray-500 line-through ml-1">₹{product.product_price}</span>}
+                    {showDiscount && <span className="text-sm text-gray-500 line-through ">₹{product.product_price}</span>}
                   </div>
                 </div>
               </div>
